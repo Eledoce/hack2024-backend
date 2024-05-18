9e14a2b1f79c19f0dca5976afe95c6c03dd559f8
eslavadev <eslava.dev@gmail.com>
2024-05-18 07:19:02 -0600

    chore: Update landing page with backend link and API documentation link

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const app = express()
 app.use('/api/v1', RouterApiV1)
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send(`
+  <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+  <h1>Backend</h1>
+
+  
+  <a href="/api/v1/docs/">Docs</a>
+
+  </div>
+  
+  `)
 })
 
 export default app
